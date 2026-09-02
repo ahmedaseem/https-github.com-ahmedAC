75c4e3337424ac3bb10ae21f93c6abe4da876be9
ahmedAC <root@gov-server.asem>
2026-09-02 17:10:57 +0200

    Fix: update country model

--- app-models_country.py
+++ app-models_country.py
@@ -85,3 +85,4 @@ class Country(db.Model):
             "is_active": self.is_active,
             "created_at": self.created_at.isoformat() if self.created_at else None,
             "updated_at": self.updated_at.isoformat() if self.updated_at else None
+
